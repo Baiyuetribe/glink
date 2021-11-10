75727bb660c8a8a6cc668ea75bd2230f4472108a
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2021-11-11 01:16:43 +0800

    fix douyin 修复抖音去水印接口

--- api/douyin.go
+++ api/douyin.go
@@ -30,14 +30,10 @@ func DouYin(url string) string {
 	if len(requrl) < 10 {
 		return ""
 	}
-
-	Itemid := strings.Split(requrl, "/")[3] ///share/video/6734643996347485448/?region=CN&mid=6734637731277851404&u_code=0&titleType=title&utm_source=copy_link&utm_campaign=client_share&utm_medium=android&app=aweme
-	// fmt.Println(res.RawResponse.Request.URL.RequestURI())
+	Itemid := regexp.MustCompile(`video/(.*?)\?`).FindStringSubmatch(requrl)[1] ///share/video/6734643996347485448/?region=CN&mid=6734637731277851404&u_code=0&titleType=title&utm_source=copy_link&utm_campaign=client_share&utm_medium=android&app=aweme
 	// reg := regexp.MustCompile(".*?video/(.*?)/")
 	// Itemid := reg.FindString(res.RawResponse.Request.URL.RequestURI())	--》 /share/video/6734643996347485448/
-	// fmt.Println(Itemid)
 	// 根据ID然后获取原始视频
-
 	infourl := "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=" + Itemid
 	res2, err := grequests.Get(infourl, &grequests.RequestOptions{
 		Headers: map[string]string{
@@ -51,7 +47,7 @@ func DouYin(url string) string {
 
 	// fmt.Println(res2.String())
 	// reg := regexp.MustCompile("video_id=(.*?)&")
-	video_id := regexp.MustCompile("video_id=(.*?)&").FindStringSubmatch(res2.String())[1] // 匹配两种结果，一种前后参数都代，另外一种只带（）里的
+	video_id := regexp.MustCompile(`vid":"(.*?)"`).FindStringSubmatch(res2.String())[1] // 匹配两种结果，一种前后参数都代，另外一种只带（）里的
 	// 拼接最终地址
 	return strings.Join([]string{"https://aweme.snssdk.com/aweme/v1/play/?video_id=", video_id, "&ratio=720p&line=0"}, "") // json返回时&符号被转换为unicode，暂无解
 }
--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	"Baiyuetribe/glink/service"
 	"log"
 
-	"github.com/gobuffalo/packr/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/filesystem"
 
 	"github.com/gofiber/fiber/v2"
 	// "github.com/gofiber/fiber/v2/middleware/logger"
@@ -21,12 +19,12 @@ func main() {
 	// app.Use(logger.New())
 	app.Use(cors.New())
 	app.Use(compress.New()) // 压缩静态资源未gzip或br
-	// app.Static("/", "web")               // 静态文件
-	app.Use("/", filesystem.New(filesystem.Config{
-		Root: packr.New("Assets Box", "/web"),
-	}))
+	app.Static("/", "web")  // 静态文件
+	// app.Use("/", filesystem.New(filesystem.Config{
+	// 	Root: packr.New("Assets Box", "/web"),
+	// }))
 	app.Get("api/*", service.ApiHandler) // 请求地址 http://127.0.0.1:3000/api/http://demo.com
-	// log.Fatal(app.Listen(":3006"))
+	log.Fatal(app.Listen(":3006"))
 	// fmt.Println("应用访问地址：http://127.0.0.1:3006")
-	log.Fatal(app.Listen("127.0.0.1:3006"))
+	// log.Fatal(app.Listen("127.0.0.1:3006"))
 }
